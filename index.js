fcd571f9c0b13bb6d78bc9348e46399faa79641d
Richard Hoffmann <r.hoffmann@crolla-lowis.de>
2016-08-25 09:42:57 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const createMiddleware = (store) => (next) => (action) => {
+  if (action.error && action.payload && action.payload.status) {
+    store.dispatch({
+      type: 'API_ERROR',
+      status: action.payload.status,
+      error: action.error,
+      message: action.payload.response
+    });
+  }
+  return next(action);
+};
+
+export default createMiddleware;
